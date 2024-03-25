8fdb4f4b7773930dc1fe8405c5257e2a1c6bed18
tokarczykbl <tokarczykbl@users.noreply.github.com>
2024-03-25 11:49:20 -0400

    Saving work

--- static/js/app.js
+++ static/js/app.js
@@ -2,11 +2,8 @@
 
 const url = "https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json";
 
-//Promise Pending
-const dataPromise = d3.json(url);
-console.log("Data Promise: ", dataPromise);
-
 //Fetching JSON data
 d3.json(url).then(function(data){
     console.log(data);
 });
+
