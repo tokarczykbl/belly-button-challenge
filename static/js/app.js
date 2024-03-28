1bfd863f5f1ccafd919eec574646c767e893c0bf
tokarczykbl <tokarczykbl@users.noreply.github.com>
2024-03-28 19:09:08 -0400

    Saving work

--- static/js/app.js
+++ static/js/app.js
@@ -3,7 +3,13 @@
 const url = "https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json";
 
 //Fetching JSON data
-d3.json(url).then(function(data){
-    console.log(data);
+d3.json(url).then(function(data) {
+  console.log("raw_json_data", data);
 });
 
+// Creating function for bar chart
+// function bar_chart(id) {
+//   d3.json(url).then(function(data) {
+//     let sample_names = data.samples.
+
+// Look at 15-Mapping Day One Exercise 10 - Stu_GeoJson
