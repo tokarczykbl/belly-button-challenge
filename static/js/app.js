99d41e7302a6cef1cafc00b5c5073dd18ecd79ff
tokarczykbl <tokarczykbl@users.noreply.github.com>
2024-03-31 15:38:20 -0400

    Saving work on dropdown

--- static/js/app.js
+++ static/js/app.js
@@ -1,15 +1,32 @@
-// Using D3 library to read in data from https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json
-
+// Assigning URL to constant variable
 const url = "https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json";
 
-//Fetching JSON data
-d3.json(url).then(function(data) {
-  console.log("raw_json_data", data);
+//  Fetching JSON data from URL
+d3.json(url).then(data => {
+    console.log(data);
 });
 
-// Creating function for bar chart
-// function bar_chart(id) {
-//   d3.json(url).then(function(data) {
-//     let sample_names = data.samples.
 
-// Look at 15-Mapping Day One Exercise 10 - Stu_GeoJson
+// Assign dropdown element in HTML to variable using D3
+let id_dropDown = d3.select("#selDataset"); 
+        
+    // Fetch data
+    d3.json(url).then(data => {
+        // Selects `names` feature in JSON data and for each name
+        data.names.forEach(name => {
+            // Append each name to the dropdown variable
+            id_dropDown.append("option").text(name).property("value", name);
+            // Console log to check names added
+            console.log(name);
+        });
+
+        // Select initial value for dropdown
+        selectedValue = (data.names[0])
+        console.log("Initial Select ", selectedValue)
+    });
+
+
+// Creating function to capture when dropdown is changed
+function optionChanged(selectedValue) {
+    console.log("Option changed: ",selectedValue);
+};
