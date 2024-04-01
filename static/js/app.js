b95413c5c3a4373490be9d629d1bee186c104fdf
tokarczykbl <tokarczykbl@users.noreply.github.com>
2024-04-01 13:51:58 -0400

    saving work

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,115 @@
 // Assigning URL to constant variable
 const url = "https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json";
 
-//  Fetching JSON data from URL
-d3.json(url).then(data => {
-    console.log(data);
-});
 
+// Building bar chart
+function barChart(selectedID) {
+    
+    // Fetch data
+    d3.json(url).then(data => {
+        // Filter JSON data based on selected ID from Dropdown
+        let selectedSample = data.samples.filter(sample => sample.id == selectedID);
+                    
+        // Save data to ID so it can be traversed into
+        let sampleData = selectedSample[0];
+        
+        // Select top 10 IDs, concate the OTU text and save to variable 
+        let topTenOtuIds = sampleData.otu_ids.slice(0, 10).map(ID => "OTU " + ID);
+            
+        // Select to 10 sample values and save to variable
+        let topTenSampleValues = sampleData.sample_values.slice(0, 10);
+                    
+        // Select to 10 otu lables and save to variable
+        let topTenLabels = sampleData.otu_labels.slice(0, 10);
+                    
+    // Build out trace for bar chart, need to reverse to adjust for plotly default
+    let trace1 = {
+        x: topTenSampleValues.reverse(),
+        y: topTenOtuIds.reverse(),
+        text: topTenLabels.reverse(),
+        type: "bar",
+        orientation: "h"
+    };
+
+    // Build out layout for bar chart,
+    let layout = {
+        yref: "container",       
+    };
+
+    Plotly.newPlot("bar", [trace1], layout);
+});
+};
+
+// Building bubble chart
+function bubbleChart(selectedID) {
+    
+    // Fetch data
+    d3.json(url).then(data => {
+        // Filter JSON data based on selected ID from Dropdown
+        let selectedSample = data.samples.filter(sample => sample.id == selectedID);
+                
+        // Save data to ID so it can be traversed into
+        let sampleData = selectedSample[0];
+        
+        let titleID = sampleData.id; 
+
+        // Select OTU IDs,
+        let bubbleOtuIds = sampleData.otu_ids;
+            
+        // Select to 10 sample values and save to variable
+        let bubbleSampleValues = sampleData.sample_values
+                    
+        // Select to 10 otu lables and save to variable
+        let bubbleLabels = sampleData.otu_labels
+                    
+    // Build out trace for bar chart, need to reverse to adjust for plotly default
+    let trace2 = {
+        x: bubbleOtuIds,
+        xlabel: "OTU",
+        y: bubbleSampleValues,
+        text: bubbleLabels,
+        mode: "markers",
+        marker: {
+            color: bubbleOtuIds,
+            size: bubbleSampleValues,
+            colorscale: 'Earth' //https://plotly.com/javascript/colorscales/
+        }
+    };
+
+    // Build out layout for bar chart,
+    let layout2 = {
+        title : {
+            text: `OTU ID ${titleID}`,
+            yanchor: "bottom",
+            y: 0.1
+    },
+        showlegend: false,
+        automargin: true
+    };
+    Plotly.newPlot("bubble", [trace2], layout2);
+});
+};
+
+//Build Demographic Info section
+function demographicInfo(selectedID) {
+    
+    d3.json(url).then((data) => {
+
+        let metadata = data.metadata.filter(mdata => mdata.id == selectedID);
+
+        let metdata_first = metadata[0];
+
+        d3.select("#sample-metadata").html("");
+
+        Object.entries(metdata_first).forEach(([key,value]) => {
+            
+            console.log("Demographic Info", key, value);
+
+            d3.select("#sample-metadata").append("p").text(`${key}: ${value}`);
+        });
+    });
+
+};
 
 // Assign dropdown element in HTML to variable using D3
 let id_dropDown = d3.select("#selDataset"); 
@@ -23,55 +127,16 @@ let id_dropDown = d3.select("#selDataset");
         // Console log the inital selected value
         console.log("Initial Select ", initialSelect);
         barChart(initialSelect)
+        bubbleChart(initialSelect)
+        demographicInfo(initialSelect)
+        washingGauge(initialSelect)
     });
-
-// Building bar chart
-function barChart(selectedID) {
-    
-    // Fetch data
-    d3.json(url).then(data => {
-        // Filter JSON data based on selected ID from Dropdown
-        selectedSample = data.samples.filter(sample => sample.id == selectedID);
-            console.log("Bar Chart Data", selectedSample[0]);
-        
-        // Save data to ID so it can be traversed into
-        sampleData = selectedSample[0];
-        
-        // Select top 10 IDs, concate the OTU text and save to variable 
-        let topTenOtuIds = sampleData.otu_ids.slice(0, 10).map(ID => "OTU " + ID);
-            //Log IDs to check work
-            console.log("Top 10 OTU IDs ", topTenOtuIds)
-
-        // Select to 10 sample values and save to variable
-        let topTenSampleValues = sampleData.sample_values.slice(0, 10);
-            //Log samples to check work 
-            console.log(topTenSampleValues)
-        
-        // Select to 10 otu lables and save to variable
-        let topTenLabels = sampleData.otu_labels.slice(0, 10);
-            console.log(topTenLabels)
-        
-    // Build out trace for bar chart, need to reverse to adjust for plotly default
-    let trace1 = {
-        x: topTenSampleValues.reverse(),
-        y: topTenOtuIds.reverse(),
-        text: topTenLabels.reverse(),
-        type: "bar",
-        orientation: "h"
-    };
-
-    // Build out layout for bar chart,
-    let layout = {
-        title: "Top 10 "  
-    };
-
-    Plotly.newPlot("bar", [trace1], layout);
-});
-};
-
 
 // Creating function to capture when dropdown is changed
 function optionChanged(value) {
     console.log("Options changed: ",value);
-    barChart(value)
+    barChart(value);
+    bubbleChart(value)
+    demographicInfo(value)
+    washingGauge(value)
     };
